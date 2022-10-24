2c3e8a05118e06b1de9496caa557b3532f108c47
ClaudioPorto <claudio.portoinfor@gmail.com>
2022-10-24 08:07:40 -0300

    Modelos e Registros

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 
+from utils.recipes.factory import make_recipe
 
 # Create your views here.
+
+
 def home(request):
-    return render(request, 'pages/home.html')
+    return render(request, 'pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(10)],
+    })
+
+
+def recipe(request, id):
+    return render(request, 'pages/recipe_detail.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True,
+    })
